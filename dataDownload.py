7fe99572d8f01dde92817a39d8c5c78335590d5d
delphin2065 <delphin2065@hotmail.com>
2025-03-05 00:08:23 +0800

    first commit

--- dataDownload.py
+++ dataDownload.py
@@ -2,8 +2,9 @@ import requests as re
 import json
 import pandas as pd
 import numpy as np
-import folium
+import streamlit as st
 
+st.cache_data(ttl=60)
 def data():
     url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
     res_j = re.get(url)
--- youbikeApp.py
+++ youbikeApp.py
@@ -20,27 +20,29 @@ btn = st.button('資料查詢')
 
 
 if btn:
+    dfq = data()
     df = dfq[dfq['sarea']==location].copy()
     df.reset_index(inplace=True, drop=True)
     latitude_mean = df[['latitude']].to_numpy()[:, 0].mean()
     longitude_mean = df[['longitude']].to_numpy()[:, 0].mean()
 
-    # 繪製中心點
-    m = folium.Map(location=[latitude_mean, longitude_mean], zoom_start=15)
+    # # 繪製中心點
+    # m = folium.Map(location=[latitude_mean, longitude_mean], zoom_start=15)
 
 
-    # 加入標記
-    arr = df[['sna', 'sarea', 'ar', 'available_return_bikes', 'latitude', 'longitude']].to_numpy()
+    # # 加入標記
+    # arr = df[['sna', 'sarea', 'ar', 'available_return_bikes', 'latitude', 'longitude']].to_numpy()
 
-    for i in arr:
-        popup_content = f"""
-            <div style='width: 300px;'>
-            {i[0]} ( {i[2]} ) 
-            <img src='{image_url}' style='width: auto; height: 15px;' alt='youbike'> {i[3]}輛可租</div>"""
-        folium.Marker(
-        location = [i[-2], i[-1]], 
-        popup = popup_content,
-        icon=folium.Icon(color='green'), 
-        ).add_to(m)
-    st_folium(m, width=700)
+    # for i in arr:
+    #     popup_content = f"""
+    #         <div style='width: 300px;'>
+    #         {i[0]} ( {i[2]} ) 
+    #         <img src='{image_url}' style='width: auto; height: 15px;' alt='youbike'> {i[3]}輛可租</div>"""
+    #     folium.Marker(
+    #     location = [i[-2], i[-1]], 
+    #     popup = popup_content,
+    #     icon=folium.Icon(color='green'), 
+    #     ).add_to(m)
+    # st_folium(m, width=700)
 
+    st.write(df)
