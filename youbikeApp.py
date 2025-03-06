a61154990d47f44ba87a298b5af4522ef59c1662
delphin2065 <delphin2065@hotmail.com>
2025-03-06 22:09:15 +0800

    first commit

--- youbikeApp.py
+++ youbikeApp.py
@@ -18,17 +18,16 @@ st.write('資料來源: YouBike2.0臺北市公共自行車即時資訊, from htt
 dfq = data()
 locations = np.unique(dfq['sarea'])
 location = st.selectbox('選擇項目', locations)
+
 btn = st.button('資料查詢')
 
 
 
 
-
 if btn:
-    maps = st.empty()
-    # texts = st.empty()
-    
-
+    texts = st.empty()
+    maps = st.empty()  
+      
     dfq = data()
     df = dfq[dfq['sarea']==location].copy()
     df.reset_index(inplace=True, drop=True)
@@ -42,15 +41,13 @@ if btn:
     # # 加入標記
     arr = df[['sna', 'sarea', 'ar', 'available_return_bikes', 'updateTime', 'latitude', 'longitude']].to_numpy()
     image_url = os.path.join(os.getcwd(), 'YouBike.png')
-    for i in arr:
-        # <img src='{image_url}' style='width: auto; height: 15px;' alt='youbike'> 
-            
+    for i in arr:  
         popup_content = f"""
             <div style='width: 250px;'>
             {i[0]} ( {i[2]} )
             <br>
             <i class="fa-solid fa-bicycle"></i> 
-            {i[3]}輛可租用</div>"""
+            {i[3]}輛可租用, {i[-3]}</div>"""
         folium.Marker(
         location = [i[-2], i[-1]], 
         popup = popup_content,
@@ -59,6 +56,9 @@ if btn:
 
     maps = folium_static(m)
 
-        
-    # texts.write(df)
-    # time.sleep(10)
+    while True:
+        dfq = data()
+        df = dfq[dfq['sarea']==location].copy()
+        df.reset_index(inplace=True, drop=True)
+        # texts.write(df)
+        time.sleep(5)
